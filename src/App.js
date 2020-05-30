25294bbb735570d835ba890262a8d373a49410d2
Maiga Boubacar <bouba.maiga99@gmail.com>
2020-05-30 02:35:18 +0000

    adding component Formulaire

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Formulaire from './components/Formulaire'
 
 class App extends Component {
   render () {
     return (
       <div className="box">
-        <h1>App</h1>
+        <div>
+          <div className="messages">
+            Message
+          </div>
+        </div>
+        <Formulaire />
       </div>
     );
   }
